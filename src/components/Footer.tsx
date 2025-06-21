af306cff57fe71198f07bc21e869f032e7d97cbb
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-21 16:45:05 +0500

    created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,9 @@ const Footer = () => {
       className="footer w-[95%] font-sans mx-auto min-h-[620px] rounded-3xl flex justify-center items-center bg-cover bg-center relative"
       style={{ backgroundImage: `url('/images/footer-image.jpg')` }}
     >
-      <div className="overlay w-full h-full bg-black/20 absolute top-0 rounded-[20px] left-0 z-10"></div>
+      <div className="overlay w-full h-full bg-black/10 absolute top-0 rounded-[20px] left-0 z-10"></div>
       <div className="w-full h-full absolute top-0 left-0 flex justify-center z-20 items-center">
-        <div className="lg:w-[90%] w-[95%] h-full flex justify-start py-5 items-center">
+        <div className="lg:w-[90%] w-[95%] h-full flex justify-start items-center">
           <div className="max-w-3xl h-auto flex flex-col gap-4 items-start relative">
             <h1 className=" lg:max-w-3xl lg:text-6xl xs:text-4xl max-sm:leading-16 leading-20 text-3xl md:text-5xl sm:text-5xl  md:max-w-xl text-white uppercase font-bold font-sans">
               Let’s Craft Your Vision Together
